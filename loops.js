5f750cda11efad3e31428c00f95855d125363b77
Johnny Soong <johnnysoong@hotmail.com>
2018-05-25 03:42:41 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,6 @@
-for (var i = 0; i < 25; i++) {
-  console.log(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
+  }
+  return array;
 }
